5530086f0b796aeaaa0c3444ae26b0c0ad80b14c
Taylor Phillips <taylorphillips133@gmail.com>
2020-06-09 21:13:36 -0700

    add the last condition for the loop to make years divisible by 400 return true as a leap year

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 // Business Logic
 function leapYear(year) {
-  if ((year % 4 === 0) && (year % 100 !== 0)) {
+  if ((year % 4 === 0) && (year % 100 !== 0) && (year % 400 === 0)) {
     return true;
   } else  {
     return false;
